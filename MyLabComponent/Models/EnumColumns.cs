15f7a94fc1991ba0d111e73b72f36ff32065b3f6
Appci11 <61198490+Appci11@users.noreply.github.com>
2024-05-28 15:27:11 +0300

    Add project files.

---
+++ MyLabComponent/Models/EnumColumns.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLabComponent.Models
+{
+    public enum EnumColumns
+    {
+        Col0 = 0,
+        Col1 = 1,
+        Col2 = 2,
+        Col3 = 3,
+        Col4 = 4,
+        Col5 = 5,
+        Col6 = 6,
+        Col7 = 7,
+        Col8 = 8,
+        Col9 = 9
+    }
+}
---
+++ MyLabComponent/Models/LabelModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLabComponent.Models
+{
+    public class LabelModel
+    {
+        public int Id { get; set; }
+        public int ColNr { get; set; }
+        public string Description { get; set; }
+        public EnumColumns Column { get; set; }
+    }
+}
